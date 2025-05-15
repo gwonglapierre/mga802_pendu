6cf5988cbf1ae2c2995e3c21913788961d8396cb
gwonglapierre <leirbag1111@gmail.com>
2025-05-15 19:17:49 -0400

    Structure du jeu relativement complète. Reste à organiser, commenter et adresser les possibles erreurs. Repasser au travers les instructions (ex: Fin de partie devra être déplacé à l'intérieur de la boucle). Probablement possible de segmenter en plus de fonctions encore.

--- fonctions.py
+++ fonctions.py
@@ -29,7 +29,7 @@ def import_mots(file_name='mots_pendu.txt'):
 
 
 def pick_mot(liste_mots):
-        '''
+    '''
     Fonction qui permet de choisir un mot au hasard parmi une liste contenant une banque de mots.
 
     Entrée:
@@ -41,4 +41,35 @@ def pick_mot(liste_mots):
     mot_choisi : Mot choisi aléatoirement dans la liste fournies.
     '''
         
-        return random.choice(liste_mots)
+    return random.choice(liste_mots)
+
+def verif_lettre(lettre,mot):
+    '''
+    Fonction qui permet de vérifier si une lettre donnée est présente dans un mot donné et, si oui, sa ou ses positions.
+
+    Entrée:
+
+    lettre : str d'un seul caractère représentant la lettre à tester
+    mot : str représentant le mot que l'utilisateur doit deviner
+
+    Sorties:
+
+    val = (bool) True : la lettre se trouve au moins une fois dans le mot / False : la lettre ne se trouve pas dans le mot
+    ind = (list) Indices  
+    '''
+
+    n_lettres = len(mot)
+    ind = []
+    val = False
+
+    for i in range(n_lettres):
+        if lettre == mot[i].upper():
+            ind.append(i)
+            val = True
+
+    if val == False:
+        print('\n\nLa lettre ne se trouve pas dans le mot!')
+    else:
+        print('\n\nLa lettre se trouve dans le mot!')
+    return val, ind
+    
--- main.py
+++ main.py
@@ -7,19 +7,84 @@ def jouer_au_pendu():
           '*********** LE JEU DU PENDU ***********\n' \
           '***************************************\n')
     
-    u_defaut = input('Souhaitez vous utiliser la banque de mots par défaut? \n' \
-    'Veuillez répondre par [Y] ou par [N]:  ')
+    # Initialisation du jeu
 
-    if u_defaut == 'N' or u_defaut == 'n':
-        file_name = input('Veuillez entrer le nom du fichier où se trouve votre banque de mots:  ')
-        liste_mots = import_mots(file_name)
-    else:
-        liste_mots = import_mots()
+    # u_defaut = input('Souhaitez vous utiliser la banque de mots par défaut? \n' \
+    # 'Veuillez répondre par [Y] ou par [N]:  ').upper()
+
+    # if u_defaut == 'N':
+    #     file_name = input('Veuillez entrer le nom du fichier où se trouve votre banque de mots:  ')
+    #     liste_mots = import_mots(file_name)
+    # else:
+    #     liste_mots = import_mots()
+
+    # Pour debug
+    liste_mots = import_mots()
+
+    # Initialisation des variables
 
     mot = pick_mot(liste_mots)
+    n_lettres = len(mot)
+    n_chances = 6
+    err_counter = list('O'*n_chances)
+    ronde = 1
+    mot_actuel = list('_' * n_lettres)
+    succes = 0
+
+    # Début de la partie
+
+    print('\n********* Début de la partie **********\n\n'\
+        f'Le mot à deviner a {n_lettres} lettres.\n'\
+        f'Vous avez droit à {n_chances} erreurs.\n'\
+        'Bonne chance!\n')
+
+    
+    # Boucle de jeu
+
+    while n_chances > 0 and succes == 0:
+
+        # Début de la boucle et affichage l'état actuel du mot
+
+        print(f'\n*************** Ronde {ronde} ***************\n\n\n'\
+              f'Chances restantes: {' '.join(err_counter)}\n'\
+                f'État actuel du mot : {' '.join(mot_actuel)}\n\n\n')
+        
+        # Demande à l'utilisateur de d'entrer une lettre
+
+        tentative = input('Veuiller entrer une lettre:  ').upper()
+        val, ind = verif_lettre(tentative,mot)
+
+        if val == True:
+            for i in ind:
+                mot_actuel[i] = tentative
+            print(' '.join(mot_actuel))
+        else:
+            n_chances -= 1
+            err_counter[-(1+n_chances)] = 'X'
+
+        if mot_actuel.count('_') == 0:
+            succes = 1
+
+        ronde += 1
+
+    
+    # Fin de partie
+
+    print('\n********** Fin de la partie ***********')
+
+
+    if succes == 0:
+        print('\n\nT\'es nul')
+    else: 
+        print('\n\nFélicitations!!!!!!!!!!!!')
+
 
 
 
     return
 
 jouer_au_pendu()
+
+# mot = 'polyvalente'
+# val, ind = verif_lettre('L',mot)
+# print(val, ind)
