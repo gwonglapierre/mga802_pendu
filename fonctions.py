bc51101d78333aa7da966ab9d99f27ffb25ca776
gwonglapierre <leirbag1111@gmail.com>
2025-05-18 22:35:08 -0400

    Sunday progress. Incorporation du fichier de lecture fourni. Debuggage et creation dict pour gérer les discreptancies des accents.

--- fonctions.py
+++ fonctions.py
@@ -6,21 +6,29 @@
 #---------------------------------------------------------------------------------------
 import random
 import os
+import string
+
+# Dictionnaire pour les accents (méthode "brute" en attendant de trouver alternative)
+# temp : liste accerts langue fr À Â Ä Ç É È Ê Ë Î Ï Ô Ö Ù Û Ü Ÿ
+dico_accents = {'À':'A', 'Â':'A', 'Ä':'A', 'Ç':'C', 'É':'E', 'È':'E', 'Ê':'E', 'Ë':'E',
+                'Î':'I', 'Ï':'I', 'Ô':'O', 'Ö':'O', 'Ù':'U', 'Û':'U', 'Ü':'U', 'Ÿ':'Y'}
+
 
 # Lecture de la banque de mots
-# Le fichier choisi doit absolument se trouver dans le même dossier que le script
 
-def import_mots(file_name='mots_pendu.txt'):
+def import_mots(file_name:str='mots_pendu.txt',dossier:str="./ressources"):
     '''
     Fonction qui permet de lire un fichier .txt contenant une banque de mots et l'importe sous la forme d'une liste
 
     Entrée:
 
-    file_name : Chaîne de caractère correspondant au nom du fichier où se trouve la banque de mots à importer
+    file_name : Chaîne de caractère correspondant au nom du fichier où se trouve la banque de mots à importer (str)
+
+    dossier : Emplacement du fichier (str)
 
     Sorties:
 
-    banque_mots : Liste contenant la banque de mots
+    banque_mots : Liste contenant la banque de mots normalisée sans les
     '''
 
     with open(file_name,mode='r') as banque_source:
@@ -28,7 +36,32 @@ def import_mots(file_name='mots_pendu.txt'):
     return banque_mots
 
 
-def pick_mot(liste_mots):
+
+
+def lire_liste_mots(fichier="mots_pendu.txt", dossier="./ressources"):
+    """Ce module permet de fournir des fonctions pour le jeu du Pendu
+    Il est écrit par Marlene Sanjose dans le cadre du cours MGA802
+    """
+    import os
+
+    # teste si le fichier existe
+    full_filename = os.path.join(dossier,fichier)
+    if not os.path.isfile(full_filename):
+        raise RuntimeError(f'Je ne trouve pas le fichier {full_filename} !')
+
+    # Ouvre le fichier contenant les mots en mode lecture
+    with open(full_filename, 'r', encoding='utf8') as f:
+        # Lire le contenu du fichier
+        words = f.read()
+
+    # Transforme la chaine de caracteres en liste
+    # le saut de ligne sert de separateur de champs
+    word_list = words.split('\n')
+
+    # retourne la liste de mots
+    return word_list
+
+def pick_mot(liste_mots:list):
     '''
     Fonction qui permet de choisir un mot au hasard parmi une liste contenant une banque de mots.
 
@@ -40,10 +73,12 @@ def pick_mot(liste_mots):
 
     mot_choisi : Mot choisi aléatoirement dans la liste fournies.
     '''
-        
-    return random.choice(liste_mots)
 
-def verif_lettre(lettre,mot):
+    mot_choisi = random.choice(liste_mots)
+        
+    return mot_choisi
+
+def verif_lettre(lettre:str,mot:str):
     '''
     Fonction qui permet de vérifier si une lettre donnée est présente dans un mot donné et, si oui, sa ou ses positions.
 
@@ -58,18 +93,51 @@ def verif_lettre(lettre,mot):
     ind = (list) Indices  
     '''
 
+    
     n_lettres = len(mot)
     ind = []
     val = False
 
+    # Boucler chacune des lettres du mot
     for i in range(n_lettres):
-        if lettre == mot[i].upper():
+        lettre_i = mot[i].upper()
+
+        # Retirer l'accent de la lettre à vérifier (si applicable)
+        if lettre_i in dico_accents.keys():
+            lettre_i = dico_accents[lettre_i]
+
+        # Comparer la lettre à valider avec la lettre indexée du mot
+        if lettre == lettre_i:
             ind.append(i)
             val = True
 
     if val == False:
-        print('\n\nLa lettre ne se trouve pas dans le mot!')
+        print('\nLa lettre ne se trouve pas dans le mot!')
     else:
-        print('\n\nLa lettre se trouve dans le mot!')
+        print('\nLa lettre se trouve dans le mot!')
     return val, ind
     
+
+## BONUS INDICE
+
+# def indice(mot_actuel:list, mot:str, track_lettres:list):
+#     '''
+#     Fonction qui permet de donner un indice à l'utilisateur s'il lui reste une chance
+
+#     Entrée:
+
+#     mot_actuel : État actuel du mot de la tentative du joueur
+#     mot : Le mot que l'utilisateur doit deviner
+#     track_lettres : Toutes les lettres qui ont été tentées par le joueur jusqu'à présent
+
+#     Sorties:
+
+#     val = (bool) True : la lettre se trouve au moins une fois dans le mot / False : la lettre ne se trouve pas dans le mot
+#     ind = (list) Indices  
+#     '''
+
+#     for i in string.ascii_uppercase
+
+
+#     return
+
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import random
+import unicodedata
 from fonctions import *
 
 def jouer_au_pendu():
@@ -18,18 +19,25 @@ def jouer_au_pendu():
     # else:
     #     liste_mots = import_mots()
 
-    # Pour debug
-    liste_mots = import_mots()
+    # Pour debug                                            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% À RETIRER %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    liste_mots = lire_liste_mots()
 
+
+    # Choix du mot 
+
+    mot = pick_mot(liste_mots).upper()
+    
     # Initialisation des variables
 
-    mot = pick_mot(liste_mots)
     n_lettres = len(mot)
     n_chances = 6
-    err_counter = list('O'*n_chances)
+    err_counter = list('O' * n_chances)
     ronde = 1
     mot_actuel = list('_' * n_lettres)
-    succes = 0
+    succes = False
+    indice_used = False
+    track_lettres = []
+    track_echecs = ''
 
     # Début de la partie
 
@@ -41,42 +49,57 @@ def jouer_au_pendu():
     
     # Boucle de jeu
 
-    while n_chances > 0 and succes == 0:
+    while n_chances > 0 and succes == False:
 
-        # Début de la boucle et affichage l'état actuel du mot
+        # Début de la boucle et affichage de l'état actuel du mot
 
-        print(f'\n*************** Ronde {ronde} ***************\n\n\n'\
+        print(f'\n*************** Ronde {ronde} ***************\n\n'\
               f'Chances restantes: {' '.join(err_counter)}\n'\
-                f'État actuel du mot : {' '.join(mot_actuel)}\n\n\n')
+              f'Lettres échouées: {track_echecs}\n\n'\
+              f'État actuel du mot : {' '.join(mot_actuel)}\n')
         
         # Demande à l'utilisateur de d'entrer une lettre
 
-        tentative = input('Veuiller entrer une lettre:  ').upper()
+        tentative = str(input('Veuiller entrer une lettre:  ').upper())
+        track_lettres.append(tentative)
         val, ind = verif_lettre(tentative,mot)
 
         if val == True:
             for i in ind:
-                mot_actuel[i] = tentative
-            print(' '.join(mot_actuel))
+                mot_actuel[i] = mot[i]
+            #print(' '.join(mot_actuel))
         else:
             n_chances -= 1
             err_counter[-(1+n_chances)] = 'X'
+            track_echecs = track_echecs+tentative+'  '
+
+        
+
 
         if mot_actuel.count('_') == 0:
-            succes = 1
+            succes = True
+
+        # BONUS INDICE 
+
+        if n_chances == 1 and indice_used == False:
+            indice_used = True
+
+            continue
 
         ronde += 1
 
-    
     # Fin de partie
 
     print('\n********** Fin de la partie ***********')
 
-
-    if succes == 0:
-        print('\n\nT\'es nul')
+    # Note : la consigne demande à ce que l'affichage du résultat de la partie soit à l'intérieur de la boucle. Je pense que cette étape est redondante avec le while et opte pour un affichage à l'extérieur de celle-ci. 
+    if succes == False:
+        print(f'\nChances restantes: {' '.join(err_counter)}\n'\
+              'Meilleure chance la prochaine fois!\n'\
+              f'Le mot à deviner était: {mot}')
     else: 
-        print('\n\nFélicitations!!!!!!!!!!!!')
+        print('\nFélicitations!!!!!!!!!!!!\n\n'\
+              f'Le mot à deviner était: {mot}')
 
 
 
@@ -84,7 +107,3 @@ def jouer_au_pendu():
     return
 
 jouer_au_pendu()
-
-# mot = 'polyvalente'
-# val, ind = verif_lettre('L',mot)
-# print(val, ind)
